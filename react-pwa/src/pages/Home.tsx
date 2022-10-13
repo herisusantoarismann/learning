2eb610ed2e004ed77dfd4e6f586a38fcc0a34709
herisusantoarismann <heri.s.arisman@gmail.com>
2022-10-13 10:50:05 +0700

    Add angular learning

--- react-pwa/src/pages/Home.tsx
+++ react-pwa/src/pages/Home.tsx
@@ -1,5 +1,47 @@
 import React from "react";
 
-const Home: React.FC = () => <h1>This is HomePage</h1>;
+interface Users {
+  name?: string;
+  username?: string;
+  email?: string;
+}
+
+const Home: React.FC = () => {
+  const [data, setData] = React.useState<Array<Users>>([{}]);
+  const [mode, setMode] = React.useState<string>("");
+
+  React.useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((res) => {
+        setMode("online");
+        setData(res);
+        localStorage.setItem("react-pwa", JSON.stringify(res));
+      })
+      .catch(() => {
+        setMode("offline");
+        const data = JSON.parse(localStorage.getItem("react-pwa") || "{}");
+        setData(data);
+      });
+  }, []);
+  return (
+    <>
+      <h1>This data from API</h1>
+      <p>You're {mode}</p>
+      <div>
+        {data.map((item) => {
+          return (
+            <>
+              <p>{item.name}</p>
+              <p>{item.username}</p>
+              <p>{item.email}</p>
+              <br />
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 export default Home;
