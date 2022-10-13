b27ae6258b00009dd13eee4e07dac3f86b6b97a2
herisusantoarismann <heri.s.arisman@gmail.com>
2022-10-13 11:40:36 +0700

    angular/add generate teams

--- angular-teamapp/src/app/app.component.ts
+++ angular-teamapp/src/app/app.component.ts
@@ -8,11 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-teamapp';
   newMemberName: string = '';
+  numberOfTeams: number | '' = 0;
   members: string[] = [];
+  teams: string[][] = [];
   errorMessage: string = '';
 
   onInput(member: string) {
     this.newMemberName = member;
+  }
+  onInputOfTeams(value: string) {
+    this.numberOfTeams = Number(value);
   }
   addMember() {
     if (!this.newMemberName) {
@@ -21,5 +26,34 @@ export class AppComponent {
     }
     this.members.push(this.newMemberName);
     this.newMemberName = '';
+  }
+  generateTeams() {
+    if (!this.numberOfTeams || this.numberOfTeams <= 0) {
+      this.errorMessage = 'Invalid number of teams';
+      return;
+    }
+    if (this.members.length < this.numberOfTeams) {
+      this.errorMessage = 'Not enough members';
+      return;
+    }
+    this.errorMessage = '';
+    const allMembers = [...this.members];
+    while (allMembers.length) {
+      for (let i = 0; i < this.numberOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random() * allMembers.length);
+        const member = allMembers.splice(randomIndex, 1)[0];
+
+        if (!member) break;
+        if (this.teams[i]) {
+          this.teams[i].push(member);
+        } else {
+          this.teams[i] = [member];
+        }
+      }
+    }
+
+    console.log(this.teams);
+    this.members = [];
+    this.numberOfTeams = '';
   }
 }
