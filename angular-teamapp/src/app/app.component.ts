bcaa645fbc3be0c7037b4e1079f50c907a19bfa4
herisusantoarismann <heri.s.arisman@gmail.com>
2022-10-13 11:16:34 +0700

    angular/add error message

--- angular-teamapp/src/app/app.component.ts
+++ angular-teamapp/src/app/app.component.ts
@@ -9,11 +9,16 @@ export class AppComponent {
   title = 'angular-teamapp';
   newMemberName: string = '';
   members: string[] = [];
+  errorMessage: string = '';
 
   onInput(member: string) {
     this.newMemberName = member;
   }
   addMember() {
+    if (!this.newMemberName) {
+      this.errorMessage = 'Name cannot be empty!';
+      return;
+    }
     this.members.push(this.newMemberName);
     this.newMemberName = '';
   }
