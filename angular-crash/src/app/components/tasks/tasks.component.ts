cc689cec42500553663929dd5ee31fc7ed8f1be3
herisusantoarismann <heri.s.arisman@gmail.com>
2022-10-21 08:51:57 +0700

    Add service task

--- angular-crash/src/app/components/tasks/tasks.component.ts
+++ angular-crash/src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { TASKS } from 'src/app/mock-tasks';
-import { Task } from 'src/app/Task';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-tasks',
@@ -8,9 +8,11 @@ import { Task } from 'src/app/Task';
   styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() {}
+  constructor(private taskService: TaskService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
 }
