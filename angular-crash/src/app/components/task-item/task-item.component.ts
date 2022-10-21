8da78d173fafbb71381a3da93dcdb7b1412c53f9
herisusantoarismann <heri.s.arisman@gmail.com>
2022-10-21 09:22:00 +0700

    Add delete task

--- angular-crash/src/app/components/task-item/task-item.component.ts
+++ angular-crash/src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from 'src/app/Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,10 +8,15 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./task-item.component.css'],
 })
 export class TaskItemComponent implements OnInit {
-  @Input() task: Task | null = null;
+  @Input() task: Task | undefined = undefined;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onDelete(task: Task | undefined) {
+    this.onDeleteTask.emit(task);
+  }
 }
